46b4a5faf897412ec307f4462f325f48d76bacdf
Rajesh Radhakrishnan <rajeshmvk@gmail.com>
2023-11-21 17:27:41 +0530

    corrected the script relative path

--- assets/js/markings/start.js
+++ assets/js/markings/start.js
@@ -3,7 +3,7 @@ class Start extends Marking {
       super(center, directionVector, width, height);
 
       this.img = new Image();
-      this.img.src = "car.png";
+      this.img.src = "{{site.baseurl}}/assets/images/car.png";
       this.type = "start";
    }
 
